1e92bbdb1e8797a91fdd2c8789d0bc86228410a0
AlistairDriscoll <pallythecoder@gmail.com>
2024-02-19 15:10:26 +0000

    Add score to increment score and wrong answers tallys

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,8 +44,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D")
+        incrementScore();
     } else {
         alert(`Aww, you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 }
 
@@ -62,12 +64,22 @@ function calculateCorrectAnswer() {
     }
 }
 
-function incrementScore() {
 
+/**
+ * Gets the old score from the DOM and increments it by 1
+ */
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText)
+    document.getElementById("score").textContent = ++oldScore;
 }
 
-function incrementWrongAnswer() {
 
+/**
+ * Gets the old wrong answers score from the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+    let oldScore = parseInt(document.getElementById("incorrect").innerText)
+    document.getElementById("incorrect").textContent = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
